7f28cb0ada6347c924b02b01b61b8ca4ea7f5a17
Pudimlixo <viestudanteslk7@gmail.com>
2026-01-04 11:22:50 -0300

    Criação de mudança de foto

--- src/scripts/perfil.js
+++ src/scripts/perfil.js
@@ -8,4 +8,23 @@ if(dados){
     nomePerfil.innerHTML = usuariosAtivo.nome;
 }
 
+let campoArquivo = document.querySelector("#arquivo");
 
+//Por que "target" e por que acesso ao índice 0 do files?
+function escolherFoto(e){
+    const arquivo = e.target.files[0] //Representa o arquivo selecionado
+
+    if(arquivo){
+        if(arquivo.type.startsWith("image/")){
+            console.log(`O arquivo é uma imagem válida`);
+            //Lógica de troca de foto -> Precisa construir a URL da imagem
+            const urlImg = URL.createObjectURL(arquivo)
+            imagemPerfil.src = urlImg;
+        } else{
+            alert(`Selecione uma imagem válida: JPG, JPEG, PNG`)
+            e.target.value = ``
+        }
+    }
+}
+
+campoArquivo.addEventListener('change', escolherFoto)
